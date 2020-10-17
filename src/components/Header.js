25e01aeb153d933c32ac14ac8f7c35ec547577e0
Jorge <jorgediazok@gmail.com>
2020-10-17 03:58:24 -0300

    header completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import PersonIcon from "@material-ui/icons/Person"
+import ForumIcon from "@material-ui/icons/Forum"
 import "../styles/Header.css"
 
 function Header() {
   return (
-    <div>
-      
+    <div className="header">
+     <PersonIcon className="header__icon" fontSize="large"/> 
+     <img className="header__logo" src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png" alt="tinder-logo"/>
+     <ForumIcon  className="header__icon" fontSize="large" />
     </div>
   )
 }
